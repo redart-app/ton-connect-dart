908dd12dac75a8e5e508ce28db6cffb34171a741
Timur Kim <allrighty1809@gmail.com>
2023-05-07 16:08:01 +0600

    initial commit

---
+++ packages/sdk/example/tonconnect_sdk_example.dart
@@ -0,0 +1,6 @@
+import 'package:tonconnect_sdk/tonconnect_sdk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/sdk/lib/tonconnect_sdk.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library tonconnect_sdk;
+
+export 'src/tonconnect_sdk_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
